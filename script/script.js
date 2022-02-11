d25286cd4d219add462b99bddb1a9b5d17fd52b6
Victor Gabriel de Lima <victorgabriel.lima03@gmail.com>
2022-02-11 00:48:13 -0300

    create window login

--- script/script.js
+++ script/script.js
@@ -1,11 +1,19 @@
 // TESTE PARA TENTAR COMMIT NO LINUX
 
+const welcome = document.querySelector(".welcome");
 const buttonPeoplesHeader = document.querySelector("header ion-icon");
 const sideBar = document.querySelector("nav");
+const overlay = document.querySelector(".overlay");
+const planeFooter = document.querySelector("footer ion-icon");
 
+function acessChat() {
+  welcome.classList.toggle("hidden");
+}
 function openAndCloseSideBar() {
   buttonPeoplesHeader.classList.toggle("hidden");
   sideBar.classList.toggle("hidden");
+  overlay.classList.toggle("hidden");
+  planeFooter.classList.toggle("hidden");
 }
 let fatherElement = null;
 function checkChoice(element) {
