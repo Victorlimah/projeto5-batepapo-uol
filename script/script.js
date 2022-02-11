efdf5cd9848205832367c46a230e557759337513
Victor Gabriel de Lima <victorgabriel.lima03@gmail.com>
2022-02-11 13:39:03 -0300

    create request login

--- script/script.js
+++ script/script.js
@@ -1,14 +1,31 @@
-// TESTE PARA TENTAR COMMIT NO LINUX
-
 const welcome = document.querySelector(".welcome");
 const buttonPeoplesHeader = document.querySelector("header ion-icon");
 const sideBar = document.querySelector("nav");
 const overlay = document.querySelector(".overlay");
 const planeFooter = document.querySelector("footer ion-icon");
+let inputUsername = document.querySelector(".welcome input");
 
 function acessChat() {
+  const username = inputUsername.value;
+  const objName = { name: username };
+  let promisseEnterChat = axios.post(
+    "https://mock-api.driven.com.br/api/v4/uol/participants",
+    objName
+  );
+  promisseEnterChat.then(sucessEnterChat);
+  promisseEnterChat.catch(failedEnterChat);
+}
+
+function sucessEnterChat(code) {
   welcome.classList.toggle("hidden");
 }
+
+function failedEnterChat(erro) {
+  if (erro.response.status === 400) {
+    inputUsername.value = "";
+  }
+}
+
 function openAndCloseSideBar() {
   buttonPeoplesHeader.classList.toggle("hidden");
   sideBar.classList.toggle("hidden");
