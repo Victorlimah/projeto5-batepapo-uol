653eee0be6c08b57917f90e7d477548f2c3303a6
Victor Gabriel de Lima <victorgabriel.lima03@gmail.com>
2022-02-10 23:29:03 -0300

    initial sidebar version

--- script/script.js
+++ script/script.js
@@ -1,1 +1,9 @@
 // TESTE PARA TENTAR COMMIT NO LINUX
+
+const buttonPeoplesHeader = document.querySelector("header ion-icon");
+const sideBar = document.querySelector('nav');
+
+function openAndCloseSideBar() {
+  buttonPeoplesHeader.classList.toggle("hidden");
+  sideBar.classList.toggle("hidden");
+}
