3fe008982eabfa65d9e3a99c160d4cdfa78c0681
Victor Gabriel de Lima <victorgabriel.lima03@gmail.com>
2022-02-10 23:58:09 -0300

    create sidebar select

--- script/script.js
+++ script/script.js
@@ -1,9 +1,25 @@
 // TESTE PARA TENTAR COMMIT NO LINUX
 
 const buttonPeoplesHeader = document.querySelector("header ion-icon");
-const sideBar = document.querySelector('nav');
+const sideBar = document.querySelector("nav");
 
 function openAndCloseSideBar() {
   buttonPeoplesHeader.classList.toggle("hidden");
   sideBar.classList.toggle("hidden");
+}
+let fatherElement = null;
+function checkChoice(element) {
+  const check = element.querySelector(".check");
+  fatherElement = element.parentNode;
+  unselectOption(fatherElement);
+  check.classList.remove("hidden");
+  check.classList.add("selected");
+}
+
+function unselectOption(element) {
+  const elementSelected = element.querySelector(".selected");
+  if (elementSelected !== null) {
+    elementSelected.classList.remove("selected");
+    elementSelected.classList.add("hidden");
+  }
 }
